d58f0e13249157d5696844825635ad92d36b8577
Vlad Sharachev <vladislav_sharachev@epam.com>
2019-10-10 14:37:29 +0400

    Algorithms try

---
+++ Gocking algorithms/src/com/epam/vladislav_sharachev/ForTry/BinarySearchTry.java
@@ -0,0 +1,28 @@
+package com.epam.vladislav_sharachev.ForTry;
+
+public class BinarySearchTry {
+    private static Integer binarySearch(int[] list, int item) {
+        int low = 0;
+        int high = list.length - 1;
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            int guess = list[mid];
+            if (guess == item) {
+                return mid;
+            }
+            if (guess > item) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println(binarySearch(list, 4));
+        System.out.println(list[4]);
+    }
+}
